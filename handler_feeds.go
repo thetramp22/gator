4d39b8b4e550fed87238c6609a01aac6295a34e8
Scott Starks <esstarks@hotmail.com>
2026-04-07 10:08:45 -0600

    refactor: moved handlerAddfeed and printFeed into handler_feeds file.

--- handler_feeds.go
+++ handler_feeds.go
@@ -3,9 +3,51 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/thetramp22/blog_aggregator/internal/database"
 )
 
-func handlerFeeds(s *state, cmd command) error {
+func handlerAddfeed(s *state, cmd command, user database.User) error {
+	if len(cmd.args) != 2 {
+		return fmt.Errorf("2 arguments required: <name> <url>")
+	}
+
+	name := cmd.args[0]
+	url := cmd.args[1]
+
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+		Url:       url,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("error creating feed: %v", err)
+	}
+
+	feedFollow, err := s.db.CreateFeedFollow(context.Background(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("error following feed: %v", err)
+	}
+
+	fmt.Printf("feed '%v' was created\n", feed.Name)
+	printFeed(feed, user)
+	fmt.Printf("feed '%v' was followed\n", feedFollow.UserName)
+
+	return nil
+}
+
+func handlerListFeeds(s *state, cmd command) error {
 	if len(cmd.args) != 0 {
 		return fmt.Errorf("no arguments required")
 	}
@@ -22,4 +64,13 @@ func handlerFeeds(s *state, cmd command) error {
 	}
 
 	return nil
+}
+
+func printFeed(feed database.Feed, user database.User) {
+	fmt.Printf("* ID:            %s\n", feed.ID)
+	fmt.Printf("* Created:       %v\n", feed.CreatedAt)
+	fmt.Printf("* Updated:       %v\n", feed.UpdatedAt)
+	fmt.Printf("* Name:          %s\n", feed.Name)
+	fmt.Printf("* URL:           %s\n", feed.Url)
+	fmt.Printf("* User:          %s\n", user.Name)
 }
--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
 	cmds.register("users", handlerListUsers)
 	cmds.register("agg", handlerAgg)
 	cmds.register("addfeed", middlewareLoggedIn(handlerAddfeed))
-	cmds.register("feeds", handlerFeeds)
+	cmds.register("feeds", handlerListFeeds)
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("following", middlewareLoggedIn(handlerFollowing))
 
